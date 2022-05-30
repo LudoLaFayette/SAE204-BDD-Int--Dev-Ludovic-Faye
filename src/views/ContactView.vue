0cc4251f2652629326ae19d8c8220b6cf45d57b9
LudoLaFayette <ludovicfaye89@gmail.com>
2022-05-31 00:31:09 +0200

    page histoire

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,78 +1,94 @@
 <template>
-     
-  <main>
-      <div class="w-[1343px] h-[2104px] relative">
-  <div
-    class="flex justify-start items-start w-[1343px] h-[2104px] absolute left-[-1px] top-[-1px] gap-2.5 p-2.5"
-  >
-    <div
-      class="self-stretch flex-grow"
-      style="background: linear-gradient(to bottom, #bbb7b4 0%, #801e1e 74.4%);"
-    ></div>
-  </div>
-  <div
-    class="flex justify-start items-start w-[695.42px] h-[152.26px] absolute left-[325.17px] top-[71.67px] gap-5 p-[5px]"
-  >
-    <p class="flex-grow w-[685.42px] text-[80px] text-center text-black">Formulaire de contact</p>
-  </div>
-  <div
-    class="flex justify-start items-start w-[964.02px] h-[235.32px] absolute left-[140.97px] top-[268.91px] gap-2.5 p-2.5"
-  >
-    <p class="flex-grow w-[944.02px] text-[64px] text-left text-white">
-      <span class="flex-grow w-[944.02px] text-[64px] text-left text-white"
-        >Besoin de plus d’informations ? </span
-      ><br /><span class="flex-grow w-[944.02px] text-[64px] text-left text-white"
-        >Posez-nous vos questions !
-      </span>
-    </p>
-  </div>
-  <div
-    class="flex justify-start items-start w-[994.55px] h-[110.74px] absolute left-[140.56px] top-[490.39px] gap-2.5 px-[15px] py-1 rounded-[10px] bg-white"
-  >
-    <p class="flex-grow w-[964.55px] text-[80px] text-left text-[#9f9f9f]">Nom</p>
-  </div>
-  <div
-    class="flex justify-start items-start w-[994.55px] h-[110.74px] absolute left-[140.56px] top-[698.03px] gap-2.5 px-[15px] py-1 rounded-[10px] bg-white"
-  >
-    <p class="flex-grow w-[964.55px] text-[80px] text-left text-[#9f9f9f]">Adresse e-mail</p>
-  </div>
-  <div
-    class="flex justify-start items-start w-[1107.07px] h-[688.64px] absolute left-[144.19px] top-[905.66px] gap-2.5 px-3 py-[9px] rounded-[10px] bg-white"
-  >
-    <p class="flex-grow w-[1083.07px] text-[80px] text-left text-[#9f9f9f]">Votre message</p>
-  </div>
-  <div
-    class="flex flex-col justify-start items-start w-[1038.1px] h-[69.21px] absolute left-[151.45px] top-[1424.74px] gap-2.5 p-2.5"
-  >
-    <svg
-      width="1019"
-      height="1"
-      viewBox="0 0 1019 1"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      class="self-stretch flex-grow-0 flex-shrink-0"
-      preserveAspectRatio="none"
-    >
-      <line
-        x1="0.448853"
-        y1="0.486816"
-        x2="1018.55"
-        y2="0.486817"
-        stroke="#95989A"
-        stroke-width="0.5"
-      ></line>
-    </svg>
-  </div>
-  <div
-    class="flex justify-start items-start w-[254.08px] h-[117.66px] absolute left-[939.83px] top-[1466.26px] gap-2.5 p-2.5"
-  >
-    <p class="flex-grow w-[234.08px] text-5xl text-center text-[#d92d3c]">ENVOYER</p>
-  </div>
-</div>
-<img src="image-5.png" class="w-[1103px] h-[698.82px] object-cover" />
-  </main>
-    
+    <div class="relative h-[2104px] w-[1343px]">
+      <div class="absolute left-[-1px] top-[-1px] flex h-[2104px] w-[1343px] items-start justify-start gap-2.5 p-2.5">
+        <div class="flex-grow self-stretch" style="background: linear-gradient(to bottom, #bbb7b4 0%, #801e1e 74.4%)"></div>
+      </div>
+      <div class="absolute left-[325.17px] top-[71.67px] flex h-[152.26px] w-[695.42px] items-start justify-start gap-5 p-[5px]">
+        <p class="w-[685.42px] flex-grow text-center text-[80px] text-black">Formulaire de contact</p>
+      </div>
+      <div class="absolute left-[140.97px] top-[268.91px] flex h-[235.32px] w-[964.02px] items-start justify-start gap-2.5 p-2.5">
+        <p class="w-[944.02px] flex-grow text-left text-[64px] text-white">
+          <span class="w-[944.02px] flex-grow text-left text-[64px] text-white">Besoin de plus d’informations ? </span><br /><span
+            class="w-[944.02px] flex-grow text-left text-[64px] text-white"
+            >Posez-nous vos questions !
+          </span>
+        </p>
+      </div>
+      <div
+        class="
+          absolute
+          left-[140.56px]
+          top-[490.39px]
+          flex
+          h-[110.74px]
+          w-[994.55px]
+          items-start
+          justify-start
+          gap-2.5
+          rounded-[10px]
+          bg-white
+          px-[15px]
+          py-1
+        "
+      >
+        <p class="w-[964.55px] flex-grow text-left text-[80px] text-[#9f9f9f]">Nom</p>
+      </div>
+      <div
+        class="
+          absolute
+          left-[140.56px]
+          top-[698.03px]
+          flex
+          h-[110.74px]
+          w-[994.55px]
+          items-start
+          justify-start
+          gap-2.5
+          rounded-[10px]
+          bg-white
+          px-[15px]
+          py-1
+        "
+      >
+        <p class="w-[964.55px] flex-grow text-left text-[80px] text-[#9f9f9f]">Adresse e-mail</p>
+      </div>
+      <div
+        class="
+          absolute
+          left-[144.19px]
+          top-[905.66px]
+          flex
+          h-[688.64px]
+          w-[1107.07px]
+          items-start
+          justify-start
+          gap-2.5
+          rounded-[10px]
+          bg-white
+          px-3
+          py-[9px]
+        "
+      >
+        <p class="w-[1083.07px] flex-grow text-left text-[80px] text-[#9f9f9f]">Votre message</p>
+      </div>
+      <div class="absolute left-[151.45px] top-[1424.74px] flex h-[69.21px] w-[1038.1px] flex-col items-start justify-start gap-2.5 p-2.5">
+        <svg
+          width="1019"
+          height="1"
+          viewBox="0 0 1019 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="flex-shrink-0 flex-grow-0 self-stretch"
+          preserveAspectRatio="none"
+        >
+          <line x1="0.448853" y1="0.486816" x2="1018.55" y2="0.486817" stroke="#95989A" stroke-width="0.5"></line>
+        </svg>
+      </div>
+      <div class="absolute left-[939.83px] top-[1466.26px] flex h-[117.66px] w-[254.08px] items-start justify-start gap-2.5 p-2.5">
+        <p class="w-[234.08px] flex-grow text-center text-5xl text-[#d92d3c]">ENVOYER</p>
+      </div>
+    </div>
+    <img src="image-5.png" class="h-[698.82px] w-[1103px] object-cover" />
 </template>
 <script>
-
 </script>
--- src/views/HistoireView.vue
+++ src/views/HistoireView.vue
@@ -1,33 +1,30 @@
 <template>
-  
-<main>
-    <div class="flex justify-start items-start w-[1350px] relative gap-2.5 p-2.5">
-  <p class="flex-grow w-[1330px] text-6xl text-center text-zinc-900">
-    <span class="flex-grow w-[1330px] text-6xl text-center text-zinc-900"
-      >Voici la partie du site ou vous pourrez découvrir l’histoire du festival. </span
-    ><br /><span class="flex-grow w-[1330px] text-6xl text-center text-zinc-900"
-      >Un festival qui prend place dans un cadre unique, au coeur de la ville de Sens.</span
-    ><br /><span class="flex-grow w-[1330px] text-6xl text-center text-zinc-900"
-      >Le marché couvert, inauguré en 1882, est un exemple typique de l’architecture métallique du
-      XIXe siècle : cette halle couverte de forme triangulaire, de style Baltard, constitue l’un des
-      rares modèles de ce type en France. En 2015, l’édifice a été rénové et a retrouvé sa splendeur
-      d’antan.</span
-    ><br /><br /><span class="flex-grow w-[1330px] text-6xl text-center text-zinc-900"
-      >Ce lieu a été choisi, car son style d’architecture, abrite un interieur riche en emplacement
-      de commerces et d’une scène centrale, dont il est possible de créer une scène locale. </span
-    ><br /><span class="flex-grow w-[1330px] text-6xl text-center text-zinc-900"
-      >Les chanteurs qui se sont rendues sur cette scène, peuvent se rappeler de la résonnance de
-      l’endroit, unique en son genre. Depuis 20 éditions, le festival peut vibrer en intérieur comme
-      en exterieur avec la place de la cathédrale ainsi que les rues adjacantes qui acceuille les
-      groupes locales lors de l’évènement “Garcon la note” en complément.</span
-    ><br />
-  </p>
+<div class="flex flex-col">
+<div class="flex">
+      <img src="../../images/image-3.webp" class="w-full" />
+    </div>
+    <!-- Changement de page pour correction à la maquette : plus de card ici -->
+    <div class="flex items-center justify-center gap-2.5 p-2.5">
+      <div class="mx-[5rem] mt-[-70px] flex rounded-[25px] border-2 border-red-600 bg-white px-[35px] py-1">
+        <h2 class="relative text-center font-alegreya-sans text-[80px] font-bold text-red-600">Histoire</h2>
+      </div>
+    </div>
+  <div class="flex items-center justify-center gap-2.5 p-2.5">
+    <p class=" flex font-alegreya-sans text-center text-6xl text-zinc-900">
+      Voici la partie du site ou vous pourrez découvrir l’histoire du festival. Un festival qui prend place dans un cadre unique, au coeur
+      de la ville de Sens. Le marché couvert, inauguré en 1882, est un exemple typique de l’architecture métallique du XIXe siècle : cette
+      halle couverte de forme triangulaire, de style Baltard, constitue l’un des rares modèles de ce type en France. En 2015, l’édifice a
+      été rénové et a retrouvé sa splendeur d’antan. 
+    </p>
+  </div>
+  <img src="../../images/image_2_1_.webp" class="w-full" />
+  <p class=" flex font-alegreya-sans text-center text-6xl text-zinc-900">Ce lieu a été choisi, car son style d’architecture, abrite un interieur riche en
+      emplacement de commerces et d’une scène centrale, dont il est possible de créer une scène locale. Les chanteurs qui se sont rendues
+      sur cette scène, peuvent se rappeler de la résonnance de l’endroit, unique en son genre. Depuis 20 éditions, le festival peut vibrer
+      en intérieur comme en exterieur avec la place de la cathédrale ainsi que les rues adjacantes qui acceuille les groupes locales lors de
+      l’évènement “Garcon la note” en complément.</p>
+  <img src="../../images/image_4.webp" class="w-full" />
 </div>
-<img src="image-2.png" class="w-[1102px] h-[691px] object-cover" />
-<img src="image-4.png" class="w-[1102px] h-[733.33px] object-cover" />
-</main>
- 
 </template>
 <script>
-
 </script>
