d7f94f7458176ad1e2e0ed48dd28f4cab9e77bab
LudoLaFayette <ludovicfaye89@gmail.com>
2022-05-30 19:37:37 +0200

    acceuil V2

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="flex flex-col bg-gradient-to-b from-grey-gradient to-red-700">
-    <div class="mx-12 flex justify-center p-6 text-center">
-      <p class="font-Alegreya Sans mx-4 flex text-center text-[100px] italic text-white">
-        “Un fesitival qui a des “h’ailles”. Un cadre unique tant historique. Profitez de la variété offerte”
+
+
+    <div class="mx-12 flex justify-center p-20 text-center">
+      <p class="font-alegreya-sans mx-4 flex text-center text-[100px] italic text-white">
+        “Un fesitival qui a des “h’ailles”.
+         Un cadre unique tant historique.
+          Profitez de la variété offerte”
       </p>
     </div>
     <div class="flex justify-center py-20">
@@ -12,70 +16,61 @@
       <FlecheBas></FlecheBas>
     </div>
 
-    <div class="flex max-w-max items-center justify-center p-2.5">
-      <p class="font-Alegreya Sans flex justify-center text-center text-6xl text-white">
+    <div class="flex items-center justify-center p-2.5">
+      <p class="font-alegreya-sans flex justify-center text-center text-6xl text-white">
         Bienvenue au festival AUX’HALLES, un festival dans les halles de Sens, dans l’Yonne, à une heure de Auxerre par le nord. Un festival
         de musique de type varéiété francaise dans un cadre unique, un hall de marché.
       </p>
     </div>
 
-    <div class="flex justify-center py-20 text-center">
-      <div
-        class="flex flex-col items-center justify-center gap-[40px] rounded-t-full rounded-b-[100px] bg-white"
-        style="box-shadow: 5px 5px 5px 0 rgba(0, 0, 0, 0.5)"
-      >
-        <img src="../../images/Image-1-1.webp" class="flex w-full rounded-t-[35px]" style="width: 100%" />
-        <p class="pl-10 text-[80px] text-black">Concerts</p>
-        <div class="flex items-center justify-center gap-2.5 px-2.5">
-          <p class="flex text-center text-[52px] text-black">
+    <div class="flex py-20 justify-center font-alegreya-sans">
+      <div class="flex w-[75%] flex-col gap-[40px] rounded-[35px] bg-white drop-shadow-sm">
+        <img src="../../images/Image-1-1-_1_.webp" class="flex w-full rounded-tr-[35px] rounded-tl-[35px]" />
+        <p class="font-Alegreya Sans pl-10 text-left text-[80px] text-black">Concerts</p>
+        <div class="flex px-5">
+          <p class="font-Alegreya Sans flex text-left text-[52px] text-black">
             Voici les concerts qui vont se proposer dans les jours qui vont arriver. Profitez et découvrez la variété française comme vous
             ne l’avez jamais vu auparavent.
           </p>
         </div>
         <div class="flex">
-          <hr class="flex w-[1000px] border-4 border-solid border-black bg-black" />
+          <hr class="flex w-[201.5vh] border-2 border-solid border-black bg-black" />
         </div>
-        <p class="ml-auto flex w-max py-10 text-5xl text-[#d92d3c]">DÉCOUVRIR</p>
+        <p class="ml-auto flex w-max py-10 pr-[50px] text-5xl text-[#d92d3c]">DÉCOUVRIR</p>
       </div>
     </div>
 
-    <div
-      class="relative flex h-[1232px] w-[936px] flex-col items-start justify-start gap-[35px] rounded-[35px] bg-white"
-      style="box-shadow: 5px 5px 5px 0 rgba(0, 0, 0, 0.5)"
-    >
-      <img src="../../images/Image-1-3_1.webp" class="flex-grow self-stretch rounded-tl-[35px] rounded-tr-[35px] object-cover" />
-      <p class="w-[936px] flex-shrink-0 flex-grow-0 self-stretch text-left text-[80px] text-black">Artistes</p>
-      <div class="relative flex flex-shrink-0 flex-grow-0 items-center justify-center gap-2.5 self-stretch px-2.5 pb-5">
-        <p class="w-[916px] flex-grow text-left text-[52px] text-black">
-          Découvrez les artistes qui particperont aux éditions de ce festival haut en couleur. Ils seront prêts à se faire entendre afin de
-          vous emmener dans le saint gr’halles de la musique.
-        </p>
-      </div>
-      <div class="flex">
-        <hr class="flex w-[1000px] border-2 border-solid border-black bg-black" />
-      </div>
-      <div class="relative flex flex-shrink-0 flex-grow-0 items-center justify-end gap-2.5 self-stretch pr-[30px] pb-[5px]">
-        <p class="w-[906px] flex-grow text-right text-5xl text-[#d92d3c]">DÉCOUVRIR</p>
+    <div class="flex py-20 justify-center font-alegreya-sans">
+      <div class="flex w-[75%] flex-col gap-[35px] rounded-[35px] bg-white drop-shadow-sm">
+        <img src="../../images/Image-1-3-_1_.webp" class="rounded-tr-[35px] rounded-tl-[35px] flex w-full" />
+        <p class="font-Alegreya Sans pl-10 text-left text-[80px] text-black">Artistes</p>
+        <div class="flex px-5 pb-5">
+          <p class="font-Alegreya Sans flex text-left text-[52px] text-black">
+            Découvrez les artistes qui particperont aux éditions de ce festival haut en couleur. Ils seront prêts à se faire entendre afin
+            de vous emmener dans le saint gr’halles de la musique.
+          </p>
+        </div>
+        <div class="flex">
+          <hr class="flex w-[201.5vh] border-2 border-solid border-black bg-black" />
+        </div>
+        <p class="ml-auto flex w-max py-10 pr-[50px] text-5xl text-[#d92d3c]">DÉCOUVRIR</p>
       </div>
     </div>
 
-    <div
-      class="relative flex h-[1232px] w-[936px] flex-col items-start justify-start gap-[35px] rounded-[35px] bg-white"
-      style="box-shadow: 5px 5px 5px 0 rgba(0, 0, 0, 0.5)"
-    >
-      <img src="../../images/image-2_1.webp" class="flex-grow self-stretch rounded-tl-[35px] rounded-tr-[35px] object-cover" />
-      <p class="w-[936px] flex-shrink-0 flex-grow-0 self-stretch text-left text-[80px] text-black">Le festival</p>
-      <div class="relative flex flex-shrink-0 flex-grow-0 items-start justify-start gap-2.5 self-stretch px-2.5">
-        <p class="w-[916px] flex-grow text-left text-[52px] text-black">
-          Venez découvrir le cadre qui se cache dérrière ce festival. Vous en appendrez tant sur les halles que l’organisateur du festival
-          et aimez le lieu unique de ce festival.
-        </p>
-      </div>
-      <div class="flex">
-        <hr class="flex w-[1000px] border-2 border-solid border-black bg-black" />
-      </div>
-      <div class="relative flex flex-shrink-0 flex-grow-0 items-center justify-end gap-2.5 self-stretch pr-[30px] pb-[5px]">
-        <p class="w-[906px] flex-grow text-right text-5xl text-[#d92d3c]">DÉCOUVRIR</p>
+    <div class="flex py-20 justify-center font-alegreya-sans">
+      <div class="flex w-[75%] flex-col gap-[35px] rounded-[35px] bg-white drop-shadow-sm">
+        <img src="../../images/image-3.webp" class="flex w-full rounded-tr-[35px] rounded-tl-[35px]" />
+        <p class="font-alegreya-sans pl-10 text-left text-[80px] text-black">Le festival</p>
+        <div class="flex px-5">
+          <p class="font-alegreya-sans flex text-left text-[52px] text-black">
+            Venez découvrir le cadre qui se cache dérrière ce festival. Vous en appendrez tant sur les halles que l’organisateur du festival
+            et aimez le lieu unique de ce festival.
+          </p>
+        </div>
+        <div class="flex">
+          <hr class="flex w-[201.5vh] border-2 border-solid border-black bg-black" />
+        </div>
+        <p class="ml-auto flex w-max py-10 pr-[50px] text-5xl text-[#d92d3c] font-alegreya-sans">DÉCOUVRIR</p>
       </div>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,27 @@ module.exports = {
         "grey-gradient" : "#A8A29E",
         // Ici vos couleurs exportées de Figma
       },
-      "fontFamily": {
-        "Alegreya Sans" : "Alegreya",
+      "fontSize": {
+        "base": "5.625rem",
+        "lg": "6rem"
+       },
+       "fontFamily": {
         "happy-times-at-the-ikob-new-game-plus-edition": "Happy Times at the IKOB New Game Plus Edition",
-        // Ici vos familles de polices exportées de Figma
-        // Ne pas oublier de les charger dans '/index.html' !
-      },
+        "alegreya-sans": "Alegreya Sans"
+       },
+       "borderRadius": {
+        "none": "0",
+        "xs": "0.25rem",
+        "sm": "0.625rem",
+        "default": "1.5625rem",
+        "lg": "2.1875rem",
+        "xl": "2.8271484375rem",
+        "2xl": "5.0888671875rem",
+        "full": "9999px"
+       },
+      
     },
+
 
   },
   plugins: [],
