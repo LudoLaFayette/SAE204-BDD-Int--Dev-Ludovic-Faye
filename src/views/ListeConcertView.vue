84ab1e91276dc2fae3a1ea1a72e6efb134b435d4
LudoLaFayette <ludovicfaye89@gmail.com>
2022-06-02 08:15:50 +0200

    liste concert dark mode

--- src/views/ListeConcertView.vue
+++ src/views/ListeConcertView.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col bg-fond">
     <img src="../../images/Image-1-1-_1_.webp" class="w-full" />
 
     <div class="flex items-center justify-center gap-2.5 p-2.5">
-      <div class="mx-[5rem] mt-[-70px] flex rounded-[25px] border-2 border-red-600 bg-white px-[35px] py-1">
-        <h2 class="text-center font-alegreya-sans lg:text-8xl md:text-6xl text-2xl font-bold text-red-600">
+      <div class="mx-[5rem] mt-[-70px] flex rounded-[25px] border-2 border--couleur-red-600 bg-fond px-[35px] py-1">
+        <h2 class="text-center font-alegreya-sans lg:text-8xl md:text-6xl text-2xl font-bold text-couleur-red-600">
           Liste des <br />
           Concerts
         </h2>
       </div>
     </div>
 
-    <p class="flex text-center md:text-4xl text-2xl text-zinc-900 mt-6">
+    <p class="flex text-center md:text-4xl text-2xl text-text mt-6">
       Voici la partie du site ou vous pourrez choisir le concert auxquel vous voulez réserver votre place :
     </p>
 
-    <div class="grid md:grid-cols-4 lg:gap-[20px] grid-cols-2 gap-2 rounded-[25px] mt-[50px] border border-white bg-gray-100">
+    <div class="grid md:grid-cols-4 lg:gap-[20px] grid-cols-2 gap-2 rounded-[25px] mt-[50px] border border-fond bg-card-2nd">
       <div class="flex flex-col justify-center">
-        <p class="text-left lg:text-3xl text-base text-black">Trier par :</p>
+        <p class="text-left lg:text-3xl text-base text-text">Trier par :</p>
       </div>
 
-      <div class="flex items-center border-b-2 border-solid border-black">
-        <p class="pt-[15px] text-left lg:text-3xl text-base text-black">Lieu de la scène</p>
-        <div class="ml-auto flex items-center  border-black pt-5 ">
+      <div class="flex items-center border-b-2 border-solid border-text">
+        <p class="pt-[15px] text-left lg:text-3xl text-base text-text">Lieu de la scène</p>
+        <div class="ml-auto flex items-center  border-text pt-5 ">
           <svg
             width="20"
             height="12"
             viewBox="0 0 34 20"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            class="ml-auto lg:w-full w-1/2"
+            class="ml-auto lg:w-full w-1/2 stroke-black dark:stroke-white"
             preserveAspectRatio="none"
           >
             <line
@@ -37,29 +37,29 @@
               x2="24.7607"
               y2="-0.5"
               transform="matrix(0.671269 0.741214 -0.671269 0.741214 0.347412 1)"
-              stroke="black"
+              
             ></line>
             <line
               y1="-0.5"
               x2="24.7607"
               y2="-0.5"
               transform="matrix(0.671269 -0.741214 0.671269 0.741214 16.9685 19.353)"
-              stroke="black"
+              
             ></line>
           </svg>
         </div>
       </div>
 
-      <div class="flex items-center border-b-2 border-solid border-black">
-        <p class="pt-[15px] text-left lg:text-3xl text-base text-black">Artiste</p>
-        <div class="ml-auto flex items-center  border-black pt-5 ">
+      <div class="flex items-center border-b-2 border-solid border-text">
+        <p class="pt-[15px] text-left lg:text-3xl text-base text-text">Artiste</p>
+        <div class="ml-auto flex items-center  border-text pt-5 ">
           <svg
             width="20"
             height="12"
             viewBox="0 0 34 20"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            class="ml-auto lg:w-full w-1/2"
+            class="ml-auto lg:w-full w-1/2 stroke-black dark:stroke-white"
             preserveAspectRatio="none"
           >
             <line
@@ -67,29 +67,29 @@
               x2="24.7607"
               y2="-0.5"
               transform="matrix(0.671269 0.741214 -0.671269 0.741214 0.347412 1)"
-              stroke="black"
+              
             ></line>
             <line
               y1="-0.5"
               x2="24.7607"
               y2="-0.5"
               transform="matrix(0.671269 -0.741214 0.671269 0.741214 16.9685 19.353)"
-              stroke="black"
+         
             ></line>
           </svg>
         </div>
       </div>
 
-      <div class="flex items-center border-b-2 border-solid border-black">
-        <p class="text-left lg:text-3xl text-base  text-black pt-[15px]">Catégorie</p>
-        <div class="ml-auto flex items-center justify-between border-black pt-5 ">
+      <div class="flex items-center border-b-2 border-solid border-text">
+        <p class="text-left lg:text-3xl text-base  text-text pt-[15px]">Catégorie</p>
+        <div class="ml-auto flex items-center justify-between border-text pt-5 ">
           <svg
             width="20 "
             height="12"
             viewBox="0 0 34 20"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            class="ml-auto lg:w-full w-1/4"
+            class="ml-auto lg:w-full w-1/4 stroke-black dark:stroke-white"
             preserveAspectRatio="none"
           >
             <line
@@ -97,14 +97,14 @@
               x2="24.7607"
               y2="-0.5"
               transform="matrix(0.671269 0.741214 -0.671269 0.741214 0.347412 1)"
-              stroke="black"
+              
             ></line>
             <line
               y1="-0.5"
               x2="24.7607"
               y2="-0.5"
               transform="matrix(0.671269 -0.741214 0.671269 0.741214 16.9685 19.353)"
-              stroke="black"
+             
             ></line>
           </svg>
         </div>
@@ -112,39 +112,39 @@
     </div>
 
     <div class="flex justify-center py-20 font-alegreya-sans">
-      <div class="flex w-[75%] flex-col gap-[40px] rounded-[35px] bg-gray-200 shadow-cardShadow">
+      <div class="flex w-[75%] flex-col gap-[40px] rounded-[35px] bg-card-2nd shadow-cardShadow">
         <img src="../../images/Image-1-1-_1_.webp" class="flex w-full rounded-tr-[35px] rounded-tl-[35px]" />
-        <p class="font-Alegreya Sans pl-10 text-left lg:text-7xl md:text-5xl text-3xl text-black">Yves Jamait</p>
+        <p class="font-Alegreya Sans pl-10 text-left lg:text-7xl md:text-5xl text-3xl text-text">Yves Jamait</p>
         <div class="flex px-5">
-          <p class="font-Alegreya Sans flex text-left lg:text-5xl md:text-3xl text-2xl text-black">
+          <p class="font-Alegreya Sans flex text-left lg:text-5xl md:text-3xl text-2xl text-text">
             Lundi 16 Juin : 14h30 - 16h30 <br />
             Lieu : Scène Interieur Halle <br />Catégorie : Chanson Française, Variété Francaise, Guitare
           </p>
         </div>
         <div class="flex">
-          <hr class="flex w-[201.5vh] border-2 border-solid border-black bg-black" />
+          <hr class="flex w-[201.5vh] border-2 border-solid border-text bg-text" />
         </div>
         <router-link to="/concert">
-        <p class="ml-auto flex w-max py-10 pr-[50px] font-alegreya-sans text-2xl text-[#d92d3c]">RESERVER</p>
+        <p class="ml-auto flex w-max py-10 pr-[50px] font-alegreya-sans text-2xl text-interactive">RESERVER</p>
         </router-link>
       </div>
     </div>
 
     <div class="flex justify-center py-20 font-alegreya-sans">
-      <div class="flex w-[75%] flex-col gap-[40px] rounded-[35px] bg-gray-200 shadow-cardShadow">
+      <div class="flex w-[75%] flex-col gap-[40px] rounded-[35px] bg-card-2nd shadow-cardShadow">
         <img src="../../images/Image-1-1-_1_.webp" class="flex w-full rounded-tr-[35px] rounded-tl-[35px]" />
-        <p class="font-Alegreya Sans pl-10 text-left lg:text-7xl md:text-5xl text-3xl text-black">Yves Jamait</p>
+        <p class="font-Alegreya Sans pl-10 text-left lg:text-7xl md:text-5xl text-3xl text-text">Yves Jamait</p>
         <div class="flex px-5">
-          <p class="font-Alegreya Sans flex text-left lg:text-5xl md:text-3xl text-2xl text-black">
+          <p class="font-Alegreya Sans flex text-left lg:text-5xl md:text-3xl text-2xl text-text">
             Lundi 16 Juin : 14h30 - 16h30 <br />
             Lieu : Scène Interieur Halle <br />Catégorie : Chanson Française, Variété Francaise, Guitare
           </p>
         </div>
         <div class="flex">
-          <hr class="flex w-[201.5vh] border-2 border-solid border-black bg-black" />
+          <hr class="flex w-[201.5vh] border-2 border-solid border-text bg-text" />
         </div>
         <router-link to="/concert">
-        <p class="ml-auto flex w-max py-10 pr-[50px] font-alegreya-sans text-2xl text-[#d92d3c]">RESERVER</p>
+        <p class="ml-auto flex w-max py-10 pr-[50px] font-alegreya-sans text-2xl text-interactive">RESERVER</p>
         </router-link>
       </div>
     </div>
